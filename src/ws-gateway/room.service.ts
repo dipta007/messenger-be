14e8429cb8018b572df1b90e9afad06bff250741
Shubhashis Roy Dipta <iamdipta@gmail.com>
2020-08-05 00:50:13 +0600

    fixed object id

--- src/ws-gateway/room.service.ts
+++ src/ws-gateway/room.service.ts
@@ -11,11 +11,11 @@ export class RoomService {
 
   async addUserToRoom(roomId: string, userId: string) {
     const existingRoom = isValidObjectId(roomId) && await this.roomModel.findById(roomId)
-    const user = await this.userModel.findById(userId);
+    // const user = await this.userModel.findById(userId);
     
     let users = [];
     users = existingRoom.users
-    users = [...users, user._id];
+    users = [...users, userId];
     users = [...new Set(users)]
 
     await existingRoom.updateOne({ users });
